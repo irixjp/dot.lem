927becb70a97fb7b4fdd1932dbd822c39602e7ad
Cloud User <centos@ip-172-31-42-199.ap-northeast-1.compute.internal>
2020-01-25 22:35:55 +0900

    first commit

---
+++ init.lisp
@@ -0,0 +1,24 @@
+(in-package :lem)
+(define-command my/other-window-1 () ()
+  (other-window -1))
+
+(define-key *global-keymap* "C-_" 'undo)
+(define-key *global-keymap* "M-_" 'redo)
+(define-key *global-keymap* "C-o" 'lem.abbrev:abbrev)
+(define-key *global-keymap* "M-o" 'other-window)
+(define-key *global-keymap* "M-O" 'my/other-window-1)
+(define-key *global-keymap* "C-c l l" 'lem-lisp-mode:start-lisp-repl)
+
+(add-hook *find-file-hook*
+          (lambda (buffer)
+            (when (eq (buffer-major-mode buffer) 'lem-lisp-mode:lisp-mode)
+              (change-buffer-mode buffer 'lem-paredit-mode:paredit-mode))))
+
+
+(in-package :lem.completion-mode)
+(define-key *completion-mode-keymap* "Tab" 'completion-select)
+
+
+(in-package :lem-paredit-mode)
+(define-key *paredit-mode-keymap* "ȣ" 'paredit-barf)
+(define-key *paredit-mode-keymap* "Ȳ" 'paredit-slurp)
